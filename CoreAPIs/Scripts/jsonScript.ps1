f0349d8fa63585db4bf5204d74f0bb7bee898785
MB-git-apimatic <91535378+MB-git-apimatic@users.noreply.github.com>
2021-09-30 13:45:13 +0530

    Added powershell

---
+++ CoreAPIs/Scripts/jsonScript.ps1
@@ -0,0 +1,4 @@
+$request = 'https://coreapisnew.azurewebsites.net/swagger/v1/swagger.json'
+$response = Invoke-WebRequest $request
+$jsonObj = ConvertFrom-Json $([String]::new($response.Content))
+$jsonObj | ConvertTo-Json -depth 32| set-content '.\APIMatic\spec\coreAPIs.json'
