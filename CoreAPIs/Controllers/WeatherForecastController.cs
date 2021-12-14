51c952069798d351bb71f053ecd0884016551001
MB-git-apimatic <91535378+MB-git-apimatic@users.noreply.github.com>
2021-12-14 18:36:38 +0530

    Added new endpoint

--- CoreAPIs/Controllers/WeatherForecastController.cs
+++ CoreAPIs/Controllers/WeatherForecastController.cs
@@ -64,5 +64,25 @@ namespace CoreAPIs.Controllers
 
             return c.Where(x => x.id == forecastId).FirstOrDefault();
         }
+
+        /// <summary>
+        /// Gets the index of the by.
+        /// </summary>
+        /// <param name="indexId">The index identifier.</param>
+        /// <returns></returns>
+        [HttpGet("index/{indexId}")]
+        public WeatherForecast GetByIndex(int indexId)
+        {
+            var rng = new Random();
+            var c = Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = rng.Next(-20, 55),
+                Summary = Summaries[rng.Next(Summaries.Length)]
+            })
+            .ToArray();
+
+            return c.Where(x => x.id == indexId).FirstOrDefault();
+        }
     }
 }
