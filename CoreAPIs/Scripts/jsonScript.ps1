1783d80e37bf579894598701f6ea636490de13a7
MB-git-apimatic <91535378+MB-git-apimatic@users.noreply.github.com>
2021-12-15 15:56:34 +0530

    updated file

--- CoreAPIs/Scripts/jsonScript.ps1
+++ CoreAPIs/Scripts/jsonScript.ps1
@@ -1,4 +1,4 @@
 $request = 'https://coreapisnew.azurewebsites.net/swagger/v1/swagger.json'
 $response = Invoke-WebRequest $request
 $jsonObj = ConvertFrom-Json $([String]::new($response.Content))
-$jsonObj | ConvertTo-Json -depth 32| set-content '.\APIMatic\spec\coreAPIs.json'
+$jsonObj | ConvertTo-Json -depth 32| set-content '.\APIMatic\spec\spec1\coreAPIs.json'
